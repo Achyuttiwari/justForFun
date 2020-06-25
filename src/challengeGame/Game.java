482741e037037eba8c13ad928aa424e630148191
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-06-25 22:13:41 +0530

    Welcome Message

---
+++ src/challengeGame/Game.java
@@ -0,0 +1,15 @@
+/*
+ *   Created by IntelliJ IDEA.
+ *   User: Tiwari
+ *   Date: 6/25/2020
+ *   Time: 10:08 PM
+ */
+package challengeGame;
+
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Game");
+    }
+}
